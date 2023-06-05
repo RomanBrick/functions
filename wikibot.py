f3543f1463634b10383d7e1245c5e84a31fefac3
RomanBrick <32520129+RomanBrick@users.noreply.github.com>
2023-06-05 17:55:51 +0000

    wikibot has been updated

--- wikibot.py
+++ wikibot.py
@@ -1,12 +1,26 @@
-from mylib.bot import scrape
+# from mylib.bot import scrape
+# import click
+
+
+# @click.command()
+# @click.option("--name", help="Web page we want to scrape")
+# def cli(name="Microsoft", lenght=1):
+#     result = scrape(name)
+#     click.echo(click.style(f"{result}:",bg="blue", fg="white", bold=True))
+
+# if __name__ == "__main__":
+#     cli()
+
 import click
+from mylib.bot import scrape
 
 
 @click.command()
 @click.option("--name", help="Web page we want to scrape")
-def cli(name="Microsoft", lenght=1):
+def cli(name):
     result = scrape(name)
-    click.echo(click.style(f"{result}:",bg="blue", fg="white", bold=True))
+    click.echo(click.style(f"{result}:", fg="blue"))
+
 
 if __name__ == "__main__":
     cli()
